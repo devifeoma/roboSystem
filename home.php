61617682482e584c2b4c77fa4e421607d2e483e9
judith <judithcynthia1@gmail.com>
2017-09-13 14:14:17 +0100

    Change the whole template to a single page, change the background image to a fixed image, change portfolio section to a carousel

--- home.php
+++ home.php
@@ -1,129 +1,268 @@
-<?php $page='home'; include('header.php');?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" href="aos-master/dist/aos.css">
+    <link rel="stylesheet" type="text/css" href="assets/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/index.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <title>Robosyslive</title>
+</head>
+<body>
 
-<div id="carousel-id" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-id" data-slide-to="0" class=""></li>
-    <li data-target="#carousel-id" data-slide-to="1" class=""></li>
-    <li data-target="#carousel-id" data-slide-to="2" class="active"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="item">
-      <img alt="First slide" src="img/robot-wallpaper.jpg" style="height: 650px; min-width: 100%;">
-      <div class="container">
-        <div class="carousel-caption">
-          <h1>Example headline.</h1>
+    <div class="front-page mp-t-20" id="home">
+        <div class="row">
+            <nav class="navbar  no-bd bd-0 " role="navigation">
+        <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand f-30" href="#" style="color: navy;"><img src="img/logo1.png" style="height: 40px;">Robotic Systems</a>
         </div>
-      </div> 
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav navbar-right f-18 no-bd">
+                 <li class="f-20"><a href="#home" style="color: navy; text-shadow: none;">Home</a></li>
+                 <li class="f-20"><a href="#about" style="color: navy; text-shadow: none;">About Us</a></li>
+                 <li class="f-20"><a href="#product" style="color: navy; text-shadow: none;">Product & Services</a></li>
+                 <li class="f-20"><a href="#portfolio" style="color: navy; text-shadow: none;">Portfolio</a></li>
+                 <li class="f-20"><a href="#contact" style="color: navy; text-shadow: none;">Contact</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
     </div>
-    <div class="item">
-      <img alt="Second slide" src="img/website-design.jpg" style="height: 650px; min-width: 100%;">
-       <div class="container">
-        <div class="carousel-caption">
-          <h1>Another example headline.</h1>
+    </nav>
         </div>
-      </div> 
-    </div>
-    <div class="item active">
-      <img alt="Third slide" src="img/website-design2.jpg" style="height: 650px; min-width: 100%;">
-       <div class="container">
-        <div class="carousel-caption">
-          <h1>One more for good measure.</h1>
+         <div class="row text-center m-t-5p">
+            <div class="col-md-2"></div>
+            <div class="col-md-8"  data-aos="zoom-in-up">
+                <p class="f-40 mf-20" style="color: navy;">Reinventing Software Security and Intelligence</p>
+            </div>
+            <div class="col-md-2"></div>
         </div>
-      </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 text-center">
+                <button type="submit" class="btn-navy f-20 c-white bd-50">Robotic Systems</button>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        </div>
+        <!-- product & services section -->
+    <div class="container-fluid " id="product">
+        <div class="container">
+            <br>
+               <h1 class="text-center" style="color: navy;">What We Offer</h1>
+               <br>
+            <div class="row">
+                <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
+                   <div class="services-icon text-center">
+                    <i class="icon-laptop"></i>
+                    <h3>Web Development</h3>  
+                   </div>
+                   <div class="description">
+                    <p class="j-ta text">If you want a website you’ll love or a professional website for a product idea then you are in the right place. We help you create long lasting and effective websites that deliver the message of your product or service efficiently. We create easy to use and update websites that you can modify yourself or even have us update it for you. With us, speed and security is a guarantee; in addition to beautiful UI, easy to navigate interfaces, mobile compatibility and unique layout.</p>
+                       
+                   </div>
+                </div>
+                   <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
+                   <div class="services-icon text-center">
+                    <i class="icon-lock"></i>
+                    <h3>Data & Office Security</h3>  
+                   </div>
+                   <div class="description">
+                    <p class="j-ta text">Do you need better security for your business? Ensure the safety and protection of your data from viruses and malwares by subscribing to our security solutions today. We can provide you with data security and backup softwares and services, as well as office security using Biometrics, Eye Scanning and Face Recognition technology. If you want to ensure your business will never experience a breakdown and/or to prevent privacy theft that may cause loss of sensitive information in your business, then you should try one of our solutions</p>
+                       
+                   </div>
+                </div>
+                   <div class="col-md-3" data-aos="fade-up">
+                        <div class="services-icon text-center">
+                            <i class="icon-cog"></i>
+                            <h3>ERP Solutions</h3>  
+                        </div>
+                   <div class="description">
+                    <p class="j-ta text">We provide enterprise resource planning software for corporations/institutions. Our solutions include robust staff management solutions, development of document processing systems, report generation, conversion of manual documents to digital forms, project and task management systems, as well as automated help provisioning or support systems. Our softwares have the capacity to transform your business into a much more efficient system in both quality and cost. </p>
+                       
+                   </div>
+                </div>
+                <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
+                    <div class="services-icon text-center">
+                        <i class="icon-user"></i>
+                        <h3 style="text-align: center;">CRM Solutions</h3>  
+                    </div>
+                   <div class="description">
+                    <p class="j-ta text">It is no doubt that we are in the Age of the Customer. Today, keeping a good record and profile of your customers is a proven strategy that help ensures you keep your clients and provide better services to them. We can provide you with customer relationship management solutions that help implement KYC (Know Your Customer) strategies and increase client satisfaction and sales.</p>
+                       
+                   </div>
+                </div>
+             
+            </div>
+        </div>
     </div>
-  </div>
-<!--   <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-  <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>  -->
+    <!-- product & services section stops -->
+    <br>
+
+    <!-- portfolio section  -->
+    <div class="container-fluid" id="portfolio" style="background-color: #f9f9f9;">
+           <div class="container">
+                <h1 class="text-center" style="color: navy;">Portfolio</h1>
+                <br>
+            <div class="row">
+                <div class="span12">
+                        <div id="myCarousel" class="carousel slide">
+                 
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                </ol>
+                 
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                    
+                <div class="item active">
+                    <div class="row-fluid">
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio1.jpg" alt="Image" style="height: 300px;" /></a></div>
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio2.jpg" alt="Image" style="height: 300px;" /></a></div>
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio3.jpg" alt="Image" style="height: 300px;" /></a></div>
+                    </div><!--/row-fluid-->
+                </div><!--/item-->
+                 
+                <div class="item">
+                    <div class="row-fluid">
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio4.jpg" alt="Image" style="height: 300px;" /></a></div>
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio5.jpg" alt="Image" style="height: 300px;" /></a></div>
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio6.jpg" alt="Image" style="height: 300px;" /></a></div>
+                    </div><!--/row-fluid-->
+                </div><!--/item-->
+                 
+                </div><!--/carousel-inner-->
+                 
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+                </div><!--/myCarousel-->
+                  
+        </div>
+    </div>
 </div> 
-  	
-
-
-  	<p class="clearfix"></p>
-    <h3 class="header">What We Offer</h3><hr>
-
-    <div class="container" id="product">
-      <div class="row">
-        <div class="col-md-3">
-          <img src="img/web_dev.png" style="height: 170px;">
-          <h4>Web Design & Development</h4>
-        </div>
-         <div class="col-md-3">
-            <img src="img/icon-security.png" style="height: 170px;">
-            <h4>Data & Office Security</h4>
-        </div>
-         <div class="col-md-3">
-          <img src="img/crm2.png" style="height: 170px;">
-          <h4>ERP Solution</h4>
-        </div>
-         <div class="col-md-3">
-          <img src="img/erp-icon.png" style="height: 170px;">
-          <h4>CRM Solutions</h4>
-        </div>
-        
-        
-      </div>
-      
     </div>
-    <p class="clearfix"></p><hr>
-    <h3 class="header">Portfolio</h3><hr>
+    <!-- portfolio section stops -->
+    <br>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-3">
-          <div class="thumbnail portfolio">
-            <a href="cruise.php"><img src="img/portfolio1.jpg" alt=""></a>
-            <div class="caption">
-              <h3>Cruise Nigeria</h3>
-              <p>Website</p>
-          </div>
-          </div>
-          
+    <!-- contact section  -->
+    <div class="container-fluid" id="contact">
+        <div class="container">
+            <div class="row">
+            <h1 class="text-center" style="color: navy;">Contact Us</h1>
+                <br>
+                <div class="col-md-5">
+                    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.040854881739!2d3.379332214111657!3d6.516513195286393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8cf8f9607795%3A0x9a95522f61f65093!2sNigerian+Stored+Products+Research+Institute!5e0!3m2!1sen!2sng!4v1504014127076" width="300" height="400" frameborder="0" style="border:0" allowfullscreen></iframe></p>
+                    
+                </div>
+                <div class="col-md-7">
+                    <form action="#" method="POST" role="form">
+                    <div class="form-group">
+                        <label for="name">Name*</label>
+                        <input type="text" class="form-control" id="" style="height: 40px;">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Email*</label>
+                        <input type="text" class="form-control" id="" style="height: 40px;">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Message*</label>
+                        <textarea name="message" id="input" class="form-control" rows="8" required="required"></textarea>
+                        <br>
+                    </div>
+                
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                    
+                </div>
+                
+            </div>
+            
         </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-3">
-          <div class="thumbnail portfolio">
-            <a href="reelfruit.php"><img src="img/portfolio2.jpg" alt=""></a>
-              <div class="caption">
-            <h3>Reelfruit</h3>
-            <p>Website</p>
-          </div>
-          </div>
         
-        </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <div class="thumbnail portfolio">
-          <a href="ccm.php"><img src="img/portfolio3.jpg" alt=""></a>
-            <div class="caption">
-          <h3>CCM Alumini Portal</h3>
-          <p>Websites</p>
-        </div>
-        </div>
-      
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-3 portfolio">
-        <div class="thumbnail portfolio ">
-          <a href="cnmedia.php"><img src="img/portfolio4.jpg" alt=""></a>
-            <div class="caption">
-          <h3>CNMedia Website</h3>
-          <p>Website</p>
-        </div>
-        </div>
-      
-      </div>
-
     </div>
-  </div>
-  	
+    <!-- contact section stops -->
+    <br>
+    <!-- footer -->
+        <div class="container-fluid" style=" background-color: #262626" id="about">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h3 style="color: white;">About Us</h3>
+                    <p class="j-ta text">Robotic Systems is an IT company that specialized in technology solutions with emphasis on security and artificial intelligence arose from a desire to solve the challenges facing the African continent and the world at large.</p>
+                                     
+                </div>
+                <div class="col-md-3">
+                    <h3 style="color: white;">Product & Services</h3>
+                    <p class="text">Web Design & Development</p>
+                    <p class="text">Data & Office security</p>
+                    <p class="text">ERP Solutions</p>
+                    <p class="text">CRM Solutions</p>
+                    
+                </div>
+                <div class="col-md-3">
+                    <h3 style="color: white;">Contact</h3>
+                    <p class="text">Email: info@judithcynthia1.com</p>
+                    <p class="text">Phone: +2348068639557</p>
+                    <p class="text">Address: No 32, Barikisu Iyede Street, 
+                    off Unilag road, Yaba, 
+                    Lagos, Nigeria</p>
+                </div>
+                <div class="col-md-3">
+                    <h3 style="color: white;">Follow Us</h3>
+                    <ul class="social-icon">
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-instagram"></i></a></li>
+                        <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                    </ul>
+                </div> 
+            </div>
+        </div>  
+        <br>
+            <br>
+            <p class="text text-center">Copyright © 2017 Robotic Systems - Reinventing Software Security and Intelligence</p>  
+            </div>
 
-  	<?php include('footer.php');?>		
-  	
-  
+            
+        </div>
+        <!-- footer stops -->
 
-  		
+   
 
+         <!-- jQuery library -->
+         <script type="text/javascript" src="aos-master/dist/aos.js"></script>
+         <script type="text/javascript">AOS.init();</script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+         <!-- Latest compiled JavaScript -->
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+         <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 
-  	
-
-
+         <script type="text/javascript">
+             $(document).ready(function() {
+            $('#myCarousel').carousel({
+            interval: 10000
+    })
+});
+         </script>
+</body>
+</html>
