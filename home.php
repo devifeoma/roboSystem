4015143cb1fe86bdd4e3f7e67997363655235eb7
judith <judithcynthia1@gmail.com>
2017-09-21 10:47:48 +0100

    Change the contact section, Add the software download, Change the font type and color

--- home.php
+++ home.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" type="text/css" href="assets/main.css">
     <link rel="stylesheet" type="text/css" href="assets/index.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700|Patua+One|Roboto+Condensed:700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <title>Robosyslive</title>
 </head>
 <body>
@@ -31,17 +33,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand f-30" href="#" style="color: navy;"><img src="img/logo1.png" style="height: 40px;">Robotic Systems</a>
+            <a class="navbar-brand f-30" href="#" style="color: #10a8b5;"><img src="img/logo1.png" style="height: 40px;">Robotic Systems</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav navbar-right f-18 no-bd">
-                 <li class="f-20"><a href="#home" style="color: navy; text-shadow: none;">Home</a></li>
-                 <li class="f-20"><a href="#about" style="color: navy; text-shadow: none;">About Us</a></li>
-                 <li class="f-20"><a href="#product" style="color: navy; text-shadow: none;">Product & Services</a></li>
-                 <li class="f-20"><a href="#portfolio" style="color: navy; text-shadow: none;">Portfolio</a></li>
-                 <li class="f-20"><a href="#contact" style="color: navy; text-shadow: none;">Contact</a></li>
+                 <li class="f-20"><a href="#home" style="color: #10a8b5; text-shadow: none;">Home</a></li>
+                 <li class="f-20"><a href="#about" style="color: #10a8b5; text-shadow: none;">About Us</a></li>
+                 <li class="f-20"><a href="#product" style="color: #10a8b5; text-shadow: none;">Services</a></li>
+                 <li class="f-20"><a href="#portfolio" style="color: #10a8b5; text-shadow: none;">Portfolio</a></li>
+                 <li class="f-20"><a href="#contact" style="color: #10a8b5; text-shadow: none;">Contact</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div>
@@ -50,80 +52,75 @@
          <div class="row text-center m-t-5p">
             <div class="col-md-2"></div>
             <div class="col-md-8"  data-aos="zoom-in-up">
-                <p class="f-40 mf-20" style="color: navy;">Reinventing Software Security and Intelligence</p>
+                <p class="f-40 mf-20" style="color: #10a8b5;">Reinventing Software Security and Intelligence</p>
             </div>
             <div class="col-md-2"></div>
         </div>
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4 text-center">
-                <button type="submit" class="btn-navy f-20 c-white bd-50">Robotic Systems</button>
             </div>
-            <div class="col-md-4"></div>
-        </div>
-        </div>
         <!-- product & services section -->
-    <div class="container-fluid " id="product">
+    <div class="container-fluid " id="product" style="background-color: #eae9e9;  padding: 20px 0px 40px 0px;">
         <div class="container">
             <br>
-               <h1 class="text-center" style="color: navy;">What We Offer</h1>
+               <h1 class="text-center" style="color: #494D4E;">What We Offer</h1>
                <br>
             <div class="row">
-                <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
-                   <div class="services-icon text-center">
+                <div class="col-md-4" data-aos="fade-up" data-aos-duration="1000">
+                   <div class="services-icon">
                     <i class="icon-laptop"></i>
-                    <h3>Web Development</h3>  
+                    <h3 style="color:#494D4E; ">Web Development</h3>  
                    </div>
                    <div class="description">
-                    <p class="j-ta text">If you want a website you’ll love or a professional website for a product idea then you are in the right place. We help you create long lasting and effective websites that deliver the message of your product or service efficiently. We create easy to use and update websites that you can modify yourself or even have us update it for you. With us, speed and security is a guarantee; in addition to beautiful UI, easy to navigate interfaces, mobile compatibility and unique layout.</p>
+                    <p class="text">If you want a website you’ll love or a professional website for a product idea then you are in the right place. We help you create long lasting and effective websites that deliver the</p>
                        
                    </div>
                 </div>
-                   <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
-                   <div class="services-icon text-center">
+                   <div class="col-md-4" data-aos="fade-right" data-aos-duration="1000">
+                   <div class="services-icon">
                     <i class="icon-lock"></i>
-                    <h3>Data & Office Security</h3>  
+                    <h3 style="color:#494D4E; ">Data & Office Security</h3>  
                    </div>
                    <div class="description">
-                    <p class="j-ta text">Do you need better security for your business? Ensure the safety and protection of your data from viruses and malwares by subscribing to our security solutions today. We can provide you with data security and backup softwares and services, as well as office security using Biometrics, Eye Scanning and Face Recognition technology. If you want to ensure your business will never experience a breakdown</p>
+                    <p class="text">Do you need better security for your business? Ensure the safety and protection of your data from viruses and malwares by subscribing to our security solutions today. We can provide you </p>
                        
                    </div>
                 </div>
-                   <div class="col-md-3" data-aos="fade-up">
-                        <div class="services-icon text-center">
+                   <div class="col-md-4" data-aos="fade-left">
+                        <div class="services-icon">
                             <i class="icon-cog"></i>
-                            <h3>ERP Solutions</h3>  
+                            <h3 style="color:#494D4E; ">ERP Solutions</h3>  
                         </div>
                    <div class="description">
-                    <p class="j-ta text">We provide enterprise resource planning software for corporations/institutions. Our solutions include robust staff management solutions, development of document processing systems, report generation, conversion of manual documents to digital forms, project and task management systems, as well as automated help provisioning or support systems.</p>
+                    <p class="text">We provide enterprise resource planning software for corporations/institutions. Our solutions include robust staff management solutions, development of document processing systems, report</p>
                        
                    </div>
                 </div>
-                <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="services-icon text-center">
+                <!-- <div class="col-md-3" data-aos="fade-up" data-aos-duration="1000">
+                    <div class="services-icon">
                         <i class="icon-user"></i>
-                        <h3 style="text-align: center;">CRM Solutions</h3>  
+                        <h3 style="text-align: center; color:#494D4E; ">CRM Solutions</h3>  
                     </div>
                    <div class="description">
-                    <p class="j-ta text">It is no doubt that we are in the Age of the Customer. Today, keeping a good record and profile of your customers is a proven strategy that help ensures you keep your clients and provide better services to them. We can provide you with customer relationship management solutions that help implement KYC (Know Your Customer) strategies and increase client satisfaction and sales.</p>
+                    <p class="j-ta text">It is no doubt that we are in the Age of the Customer. Today, keeping a good record and profile of your customers is a proven strategy that help ensures you keep your clients and provide</p>
                        
                    </div>
-                </div>
+                </div> -->
              
             </div>
         </div>
     </div>
     <!-- product & services section stops -->
+
+
     <br>
 
     <!-- portfolio section  -->
-    <div class="container-fluid" id="portfolio" style="background-color: #f9f9f9;">
+    <div class="container-fluid" id="portfolio">
            <div class="container">
-                <h1 class="text-center" style="color: navy;">Portfolio</h1>
+                <h1 class="text-center" style="color: #494D4E;">Portfolio</h1>
                 <br>
             <div class="row">
                 <div class="span12">
-                        <div id="myCarousel" class="carousel slide">
+                    <div id="myCarousel" class="carousel slide">
                  
                 <ol class="carousel-indicators">
                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -135,19 +132,19 @@
                     
                 <div class="item active">
                     <div class="row-fluid">
-                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio1.jpg" alt="Image" style="height: 300px;" /></a></div>
-                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio2.jpg" alt="Image" style="height: 300px;" /></a></div>
-                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio3.jpg" alt="Image" style="height: 300px;" /></a></div>
-                    </div><!--/row-fluid-->
-                </div><!--/item-->
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio1.jpg" alt="Image" style="height: 200px;" /></a></div>
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio2.jpg" alt="Image" style="height: 200px;" /></a></div>
+                      <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio3.jpg" alt="Image" style="height: 200px;" /></a></div>
+                    </div>
+                </div>
                  
                 <div class="item">
                     <div class="row-fluid">
-                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio4.jpg" alt="Image" style="height: 300px;" /></a></div>
-                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio5.jpg" alt="Image" style="height: 300px;" /></a></div>
-                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio6.jpg" alt="Image" style="height: 300px;" /></a></div>
-                    </div><!--/row-fluid-->
-                </div><!--/item-->
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio4.jpg" alt="Image" style="height: 200px;" /></a></div>
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio5.jpg" alt="Image" style="height: 200px;" /></a></div>
+                        <div class="span4"><a href="#x" class="thumbnail"><img src="img/portfolio6.jpg" alt="Image" style="height: 200px;" /></a></div>
+                    </div>
+                </div>
                  
                 </div><!--/carousel-inner-->
                  
@@ -159,38 +156,91 @@
     </div>
 </div> 
     </div>
-    <!-- portfolio section stops -->
+
     <br>
+    <div class="container-fluid" style="background-color: #eae9e9; padding: 20px 0px 40px 0px;">
+        <div class="container">
+            <h1 class="text-center" style="color: #494D4E;">Get Your Software Here </h1>
+            <div class="row">
+                <div class="col-md-6" data-aos="zoom-in-up">
+                   <img src="img/robosam.png" style="height: 300px; width: 500px; "> 
+                </div>
+                <div class="col-md-6" data-aos="fade-left">
+                    <h3 class="text-center" style="color: #494D4E;">Robosys Security Anti-Malware</h3>
+                    <p class="text">RoboSAM is a super light-weight antivirus program made mainly for optimum fool proof security in your USB/Flash drives. Enjoy extra features like damaged files/folders repair, unique usb recognition and password control, drive autorun immunization, etc.</p>
+                    <button type="button" class="btn download">Download Now</button>
+                    
+                </div>
+                
+            </div>
+               <div class="row">
+                <div class="col-md-6" data-aos="fade-right">
+                    <h3 class="text-center" style="color: #494D4E;">Modern Task Scheduler</h3>
+                    <p class="text">MTS is a program that allows users to perform tasks such as sending SMS, Connecting to Internet, etc. with their modems and also automate/schedule these tasks with ease and convenience. MTS currently supports ZTE and Huawei modems.</p>
+                    <button type="button" class="btn download">Download Now</button>
+                    
+                </div>
+                <div class="col-md-6" data-aos="zoom-in-up">
+                   <img src="img/mts.png" style="height: 300px; width: 300px; "> 
+                </div>
+               
+                
+                
+            </div>
+        </div>
+        
+    </div>
 
     <!-- contact section  -->
     <div class="container-fluid" id="contact">
         <div class="container">
             <div class="row">
-            <h1 class="text-center" style="color: navy;">Contact Us</h1>
+            <h1 style="color: #494D4E;">Contact Information</h1>
                 <br>
                 <div class="col-md-5">
-                    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.040854881739!2d3.379332214111657!3d6.516513195286393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8cf8f9607795%3A0x9a95522f61f65093!2sNigerian+Stored+Products+Research+Institute!5e0!3m2!1sen!2sng!4v1504014127076" width="300" height="400" frameborder="0" style="border:0" allowfullscreen></iframe></p>
+                   <div>
+                    <h3 style="color: #494D4E;">Our Office</h3>
+                        <p class="text"> No 32, Barikisu Iyede Street, 
+                    off Unilag road, Yaba, 
+                    Lagos, Nigeria</p>
+                       
+                   </div>
+                   <div>
+                    <h3 style="color: #494D4E;">Contact on</h3>
+                    <p class="text">Email: info@judithcynthia1.com</p>  <p class="text">Call us on: +2348068639557, +234589698977398</p>
+                       
+                   </div>
+                   <div>
+                    <h3 style="color: #494D4E;">Follow us on</h3>
+                    <ul class="social-icon">
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-instagram"></i></a></li>
+                        <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                    </ul>
+                       
+                   </div>
                     
                 </div>
                 <div class="col-md-7">
                     <form action="#" method="POST" role="form">
                     <div class="form-group">
-                        <label for="name">Name*</label>
-                        <input type="text" class="form-control" id="" style="height: 40px;">
+                       <!--  <label for="name" style="color: #10a8b5;">Name:</label> -->
+                        <input type="text" class="form-control" id="" placeholder="Your Name" style="height: 40px;">
                     </div>
 
                     <div class="form-group">
-                        <label for="name">Email*</label>
-                        <input type="text" class="form-control" id="" style="height: 40px;">
+                       <!--  <label for="name" style="color: #10a8b5;">Email:</label> -->
+                        <input type="text" class="form-control" id="" placeholder="Your Email" style="height: 40px;">
                     </div>
 
                     <div class="form-group">
-                        <label for="message">Message*</label>
-                        <textarea name="message" id="input" class="form-control" rows="8" required="required"></textarea>
+                       <!--  <label for="message" style="color: #10a8b5;">Message:</label> -->
+                        <textarea name="message" id="input" class="form-control" rows="8" required="required" placeholder="Drop your message"></textarea>
                         <br>
                     </div>
                 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn download">Submit</button>
                 </form>
                     
                 </div>
@@ -203,7 +253,7 @@
     <!-- contact section stops -->
     <br>
     <!-- footer -->
-        <div class="container-fluid" style=" background-color: #262626" id="about">
+        <div class="container-fluid" style=" background-color: #33312c; padding-top: 25px;" id="about">
             <div class="container">
             <div class="row">
                 <div class="col-md-3">
@@ -219,14 +269,14 @@
                     <p class="text">CRM Solutions</p>
                     
                 </div>
-                <div class="col-md-3">
+              <!--   <div class="col-md-3">
                     <h3 style="color: white;">Contact</h3>
                     <p class="text">Email: info@judithcynthia1.com</p>
                     <p class="text">Phone: +2348068639557</p>
                     <p class="text">Address: No 32, Barikisu Iyede Street, 
                     off Unilag road, Yaba, 
                     Lagos, Nigeria</p>
-                </div>
+                </div> -->
                 <div class="col-md-3">
                     <h3 style="color: white;">Follow Us</h3>
                     <ul class="social-icon">
@@ -236,6 +286,10 @@
                         <li><a href="#"><i class="icon-google-plus"></i></a></li>
                     </ul>
                 </div> 
+                <div class="col-md-3">
+                         <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.040854881739!2d3.379332214111657!3d6.516513195286393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8cf8f9607795%3A0x9a95522f61f65093!2sNigerian+Stored+Products+Research+Institute!5e0!3m2!1sen!2sng!4v1504014127076" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></p>
+                    
+                </div>
             </div>
         </div>  
         <br>
